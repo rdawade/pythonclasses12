7cba01906840ae7277b72b7a1383684aacca0742
Ravi <dawaderavi4704@gmail.com>
2023-07-27 06:56:10 +0530

    adding new file 13

---
+++ pythonclasses13.py
@@ -0,0 +1,23 @@
+### Git Github basic
+### Cloning repository
+
+### git clone "url"
+
+### what is branch
+### need of different branch
+### creating branches   git checkout -b "branch name"
+### checking the status - git status
+### to add your files - git add "filename"
+### check - git status
+###  To add origin - git remote add origin "url"
+### to check origin  - git remote -v
+### to change the origin - git remote set-url "url"
+
+### push changes - git push -u origin "branch name"
+
+
+def func(a,b):
+    if type(a) == int:
+        return "integer values",a
+    else:
+        print("please provide interger as first value")
