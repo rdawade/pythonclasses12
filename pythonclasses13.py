04ab1d3efacf6be0fb3f2b0824f667478ba78f38
Ravi <dawaderavi4704@gmail.com>
2023-07-27 07:03:50 +0530

    added some more comments

--- pythonclasses13.py
+++ pythonclasses13.py
@@ -5,15 +5,23 @@
 
 ### what is branch
 ### need of different branch
-### creating branches   git checkout -b "branch name"
+### creating branches   git checkout -b "branch name"  , git checkout "branch name"
 ### checking the status - git status
 ### to add your files - git add "filename"
 ### check - git status
+### commiting changes - git commit -m "adding my changes"
+### push changes - git push -u origin "branch name"
+
+
+
+
 ###  To add origin - git remote add origin "url"
 ### to check origin  - git remote -v
 ### to change the origin - git remote set-url "url"
 
 ### push changes - git push -u origin "branch name"
+
+
 
 
 def func(a,b):
