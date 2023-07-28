da886868d5cb948a0ff947b48ca6ea32846ad7ef
Ravi <dawaderavi4704@gmail.com>
2023-07-28 06:38:24 +0530

    adding line 345

--- pythonclasses14.py
+++ pythonclasses14.py
@@ -2,6 +2,12 @@
 ### 1. clone the repository -- git clone https://github.com/rdawade/pythonclasses12.git
 ### 2. create a new branch -- git checkout -b "branch name"
 
+### 3. add files while staying inside newly created branch -- git ad "filename"
+
+### 4. commit the files -- git commit -m "comments"
+
+### 5. push the changes -- git push -u origin "branch name"
+
 
 
 class Class14:
