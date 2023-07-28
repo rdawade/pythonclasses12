4e2d38fb5749874f9d0011b90a9f9f4594ee9411
Ravi <dawaderavi4704@gmail.com>
2023-07-28 06:29:32 +0530

    adding pythonclasses14 file

---
+++ pythonclasses14.py
@@ -0,0 +1,13 @@
+### Git and Github
+### 1. clone the repository -- git clone https://github.com/rdawade/pythonclasses12.git
+### 2. create a new branch -- git checkout -b "branch name"
+
+
+
+class Class14:
+
+    def __init__(self) -> None:
+        pass
+
+    def method(self):
+        print("class 14")
